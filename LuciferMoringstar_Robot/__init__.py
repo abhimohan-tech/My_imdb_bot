3ad38db4f6699d7b2cac017cd94fa2927400771f
Percy <percy@railway.app>
2021-10-02 13:14:41 +0000

    Initial Commit

---
+++ LuciferMoringstar_Robot/__init__.py
@@ -0,0 +1,41 @@
+from .Utils import (
+   get_filter_results,
+   get_file_details,
+   is_subscribed,
+   get_poster,
+   Media
+)
+from .Channel import (
+   RATING,
+   GENRES
+)
+
+HELP = """
+Basic Commands
+
+/start :- check am Alive
+
+Bot Owner Only
+/broadcast Reply Any Message or Media
+/stats user Status
+/ban_user  click ban_user more info
+/unban_user click unban_user more info
+/banned_users Banned User Details
+/total How Many Files Added In Database
+/logger  Get Logs
+/delete Delete File From Database
+
+@ff_new_movies_group
+"""
+
+ABOUT = """
+➥ Name :  FF Autofilter Bot
+    
+➥ Creator : [Master](t.me/ff_new_movies_group)
+
+➥ Language : Python3
+
+➥ Library : Pyrogram Asyncio 1.13.0
+
+➥ Source Code : [Click Me](https://t.me/AdhavaaBiriyaniKittiyalo)
+"""
